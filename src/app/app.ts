14d191a0561e4832d1c6c3d283949c84172e9ad0
alisameta <alisameta09@gmail.com>
2025-07-16 12:56:32 +0300

    2. Подключит сервер и загрузит тестовые данные

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ProfileCard} from '../app/common-ui/profile-card/profile-card';
+import { ProfileCard } from '../app/common-ui/profile-card/profile-card';
+import { ProfileService } from './data/services/profile.service';
+import { JsonPipe } from '@angular/common';
+import { Profile } from './data/interfaces/profile.interface';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCard],
+  imports: [
+    RouterOutlet, 
+    ProfileCard, 
+    JsonPipe
+  ],
+
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('tik-talk');
+
+  profiles:  Profile[] = [];
+  profileService = inject(ProfileService);
+
+  constructor() {
+    this.profileService.getTestAccounts()
+      .subscribe(val => {
+        this.profiles = val
+      });
+  }
 }
