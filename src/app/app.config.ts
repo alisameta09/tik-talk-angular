ae7e17f7ef66e49bab89ce6af22069dc8d0bcb9c
alisameta <alisameta09@gmail.com>
2025-07-17 14:14:58 +0300

    12. Создаст interceptor для токена

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authTokenInterceptor } from './auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(
+      withInterceptors([authTokenInterceptor]),
+      withFetch()
+    )
   ]
 };
