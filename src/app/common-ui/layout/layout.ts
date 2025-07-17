8cffb500bcba718dbf70e4ca6688bf778d6d2ace
alisameta <alisameta09@gmail.com>
2025-07-17 16:02:27 +0300

    13. Получит инфо о user и настроит refresh token

--- src/app/common-ui/layout/layout.ts
+++ src/app/common-ui/layout/layout.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from "../sidebar/sidebar";
+import { ProfileService } from '../../data/services/profile.service';
 
 @Component({
   selector: 'app-layout',
@@ -8,6 +9,14 @@ import { Sidebar } from "../sidebar/sidebar";
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
-export class Layout {
 
+export class Layout {
+  profileService = inject(ProfileService);
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    this.profileService.getMe().subscribe(val => {
+      console.log(val);
+    })
+  }
 }
