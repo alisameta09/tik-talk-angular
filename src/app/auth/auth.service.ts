65bb07268257131f8a39eac41bbebf6f1a6d232c
alisameta <alisameta09@gmail.com>
2025-07-17 13:54:13 +0300

    11. Перенаправит auth user и сохранит токен

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,18 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { TokenResponse } from './auth.interface';
 import { tap } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
   http = inject(HttpClient);
+  cookieService = inject(CookieService);
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
 
   token: string | null = null;
   refreshToken: string | null = null;
 
   get isAuth() {
+    if (!this.token) {
+      this.token = this.cookieService.get('token');
+    }
+    
     return !!this.token;
   }
 
@@ -30,6 +37,9 @@ export class AuthService {
       tap(val => {
         this.token = val.access_token;
         this.refreshToken = val.refresh_token;
+
+        this.cookieService.set('token', this.token);
+        this.cookieService.set('refreshToken', this.refreshToken);
       })
     )
   }
