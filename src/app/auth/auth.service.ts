d3bf4946de76718cdc47ff23473cc7ae020f1786
alisameta <alisameta09@gmail.com>
2025-07-17 13:12:23 +0300

    9. Подключит токен

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { TokenResponse } from './auth.interface';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +10,27 @@ export class AuthService {
   http = inject(HttpClient);
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
 
+  token: string | null = null;
+  refreshToken: string | null = null;
+
+  get isAuth() {
+    return !!this.token;
+  }
+
   login(payload: { username: string, password: string }) {
     const fd = new FormData();
 
     fd.append('username', payload.username);
     fd.append('password', payload.password);
 
-    return this.http.post(
+    return this.http.post<TokenResponse>(
       `${this.baseApiUrl}token`,
       fd
+    ).pipe(
+      tap(val => {
+        this.token = val.access_token;
+        this.refreshToken = val.refresh_token;
+      })
     )
   }
 }
