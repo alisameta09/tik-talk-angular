41faec4979b774bf6a0e21a082893ff97aa9e0ad
alisameta <alisameta09@gmail.com>
2025-07-18 12:54:37 +0300

    14. Настроит сигнал для кнопки показать пароль

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, WritableSignal, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
 import { Router } from '@angular/router';
@@ -11,9 +11,12 @@ import { Router } from '@angular/router';
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss'
 })
+
 export class LoginPage {
   authService = inject(AuthService);
   router = inject(Router);
+
+  isPasswordVisible: WritableSignal<boolean> = signal<boolean>(false);
 
   form = new FormGroup({
     username: new FormControl(null, Validators.required),
