1261924af26f449ffda2c7270df7e5ae0ab1d56c
alisameta <alisameta09@gmail.com>
2025-07-18 17:37:23 +0300

    16. Добавит остальные элементы sidebar

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,11 +1,32 @@
+
 import { Component } from '@angular/core';
+import { SvgIcon } from '../svg-icon/svg-icon';
+import { SubscriberCard } from "./subscriber-card/subscriber-card";
+import { RouterLink } from '@angular/router';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url-pipe';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [SvgIcon, SubscriberCard, RouterLink, ImgUrlPipe],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss'
 })
 export class Sidebar {
-
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: ''
+    },
+    {
+      label: 'Чаты',
+      icon: 'chats',
+      link: 'chats'
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search'
+    }
+  ]
 }
