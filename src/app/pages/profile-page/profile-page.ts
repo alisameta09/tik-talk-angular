01f04202f9ba3945947ebb27e2236799b1a6119b
alisameta <alisameta09@gmail.com>
2025-07-22 16:19:51 +0300

    21. Наcтроит header

--- src/app/pages/profile-page/profile-page.ts
+++ src/app/pages/profile-page/profile-page.ts
@@ -1,14 +1,18 @@
 import { ProfileService } from './../../data/services/profile.service';
 import { Component, inject } from '@angular/core';
 import { Header } from '../../common-ui/header/header';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [Header, AsyncPipe],
+  imports: [
+    Header, 
+    AsyncPipe,
+    RouterLink
+  ],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.scss'
 })
